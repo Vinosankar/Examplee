a73aae4f48ec3e48f287b97a3ac613c108308686
Vinosankar <80378620+Vinosankar@users.noreply.github.com>
2021-05-11 20:07:53 +0530

    example

--- examole/src/main/java/examole/sample.java
+++ examole/src/main/java/examole/sample.java
@@ -3,14 +3,17 @@ package examole;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import jdk.internal.org.objectweb.asm.tree.analysis.Value;
+
 public class sample {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
 		System.setProperty("webdriver.chrome.driver", "D:\\selenium\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.get("https:www.google.com");
+		
+	
 
 	}
 
